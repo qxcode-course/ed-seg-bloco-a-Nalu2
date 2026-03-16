46aace4f5a29d1f5761fba65b9dddd7e99759226
AnaLuiza <silvaanaluiza120@gmail.com>
2026-03-16 10:22:40 -0300

    tarefa "mumia" concluida

--- ed/mumia/go/draft.go
+++ ed/mumia/go/draft.go
@@ -1,5 +1,22 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+	var nome string
+	var idade int
+	fmt.Scan(&nome, &idade)
+	fmt.Printf("%s eh ", nome)
+	if idade < 12 {
+		fmt.Println("crianca")
+	} else if idade < 18 {
+		fmt.Println("jovem")
+	} else if idade < 65 {
+		fmt.Println("adulto")
+	} else if idade < 1000 {
+		fmt.Println("idoso")
+	} else {
+		fmt.Println("mumia")
+	}
+
 }
